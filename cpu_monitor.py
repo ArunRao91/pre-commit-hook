e23b9c3d189df2ec19369b1aeb93a0829cf0453c
ArunRao91 <166577193+ArunRao91@users.noreply.github.com>
2024-04-10 08:42:28 +0530

    Remove print from collect_metrics

--- cpu_monitor.py
+++ cpu_monitor.py
@@ -3,9 +3,7 @@ import psutil
 import time
 
 request_count = Counter('http_req_total', 'HTTP Requests Total')
-
 cpu_usage_percent = Gauge('cpu_usage_percent', 'CPU Usage Percentage')
-
 memory_usage = Gauge('memory_usage_in_bytes', 'System Memory Usage')
 
 def collect_metrics():
@@ -15,11 +13,7 @@ def collect_metrics():
             memory_bytes = psutil.virtual_memory().used
             memory_usage.set(memory_bytes)
             cpu_percent = psutil.cpu_percent(interval=1)
-            cpu_usage_percent.set(cpu_percent)
-
-            print("memory_bytes:::::: ", memory_bytes)
-            print("cpu_percent:::::: ", cpu_percent)
-            
+            cpu_usage_percent.set(cpu_percent)   
         except Exception as e:
             print(f"Error collecting metrics: {e}")
         finally:
